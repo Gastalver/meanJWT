6a25f116bbd9965078c094180f5b1a05ab5093ad
Gastalver <mgastalver@gmail.com>
2018-06-09 01:31:23 +0200

    Rutas api/registro y api/acceso

--- api/controladores/usuario.js
+++ api/controladores/usuario.js
@@ -17,43 +17,102 @@ function prueba (req,res){
     res.status(200).send({mensaje: 'Prueba del handler /prueba'})
 }
 
-// Handler de la ruta POST /usuario
-function guardarUsuario(req,res){
+// Handler de la ruta POST /registro
+function registrarUsuario(req,res){
     let enBody = req.body;
     let usuario = new Usuario();
     if(enBody.nombre && enBody.apellidos && enBody.apodo && enBody.email && enBody.clave){
         // TODO Usar sintaxis ES6 de asignación de propiedades de objetos.
         usuario.nombre = enBody.nombre;
         usuario.apellidos = enBody.apellidos;
-        usuario.apodo = enBody.apodo;
-        usuario.email = enBody.email;
+        usuario.apodo = enBody.apodo.toLowerCase();
+        usuario.email = enBody.email.toLowerCase();
         usuario.rol = 'ROL_USUARIO' // Por defecto.
         usuario.imagen = null; // Por defecto
-        // Encriptamos clave y tras terminar (en el propio callback de bcrypt) guardamos el usuario.
-        bcrypt.hash(enBody.clave,null,null,(err,hash)=>{
-            usuario.clave = hash;
-            // Guardamos el usuario y gestionamos el callback de .save, que devuelve error, o usuarioGuardado.
-            usuario.save(
-                (err,usuarioGuardado)=>{
-                if (err) {
-                    return res.send.status(500).send({mensaje: 'Error al guardar el usuario.'});
-                }
-                if(usuarioGuardado){
-                    res.status(200).send({usuario: usuarioGuardado})
-                }else{
-                    res.status(404).send({mensaje: 'No se ha registrado el usuario'})
-                }
-            })
+        // Comprobamos si el email o el apodo ya existe y continuamos en el callback de find, para ordenar asincronicidad.
+        Usuario.find({$or: [
+                {email: usuario.email},
+                {apodo: usuario.apodo}
+            ]}).exec((err,usuarios)=>{
+            if (err) return res.send.status(500).send({mensaje: 'Error al comprobar duplicados.'});
+            if (usuarios && usuarios.length >=1){
+                return res.status(200).send({mensaje:'Ya existe un usuario con ese email o apodo'});
+            }else{
+                // Encriptamos clave y tras terminar (en el propio callback de bcrypt) guardamos el usuario.
+                bcrypt.hash(enBody.clave,null,null,(err,hash)=>{
+                    usuario.clave = hash;
+                    // Guardamos el usuario y gestionamos el callback de .save, que devuelve error, o usuarioGuardado.
+                    usuario.save(
+                        (err,usuarioGuardado)=>{
+                            if (err) return res.send.status(500).send({mensaje: 'Error al guardar el usuario.'});
+                            if(usuarioGuardado){
+                                res.status(200).send({usuario: usuarioGuardado})
+                            }else{
+                                res.status(404).send({mensaje: 'No se ha registrado el usuario'})
+                            }
+                        })
+                })
+            }
+
         })
+
+
 
     }else{
         res.status(200).send({mensaje: 'Envía todos los campos necesarios.'})
     }
 }
 
+// Handler de la ruta POST /acceso
+function autenticarUsuario(req,res){
+    var reqBody = req.body;
+    var email = reqBody.email;
+    var clave = reqBody.clave;
+    // Primero buscamos si existe un usuario con el email recibido.
+    Usuario.findOne({email: email},(error,usuario)=>{
+            if (error) {
+                debug('autenticarUsuario=> error al hacer findOne: ' + error);
+                return res.send.status(500).send({mensaje: 'Error al comprobar email.'});
+            }
+            // Si existe un usuario con ese email, comparamos si la clave enviada se corresponde con la clave del usuario registrado.
+            // Continua la ejecución en el callback de bcrypt.
+            if (usuario){
+                bcrypt.compare(clave),usuario.clave,(err,coincidencia)=>{
+                    if (err){
+                        debug('autenticarUsuario=> error al desencriptar la clave para comparar: ' + err);
+                        return res.send.status(500).send({mensaje: 'Error al comprobar la clave.'});
+                    }
+                    // si la clave enviada se corresponde con la clave del usuario registrado
+                    if(coincidencia){
+                        // Disponemos una respuesta condicional, dependiendo de si reqBody contiene un parámetro enviarToken o no.
+                        // Si contiene enviarToken generamos y devolvemos un token JWT que contiene todos los datos del usuario.
+                        if(reqBody.enviarToken){
+                            return res.status(200).send({
+                                token: jwt.createToken(usuario)  //TODO Se podría encriptar el token.
+                            });
+                         //Si no contiene enviarToken devolvemos los datos de usuario, quitando antes la clave.
+                        } else {
+                            usuario.clave = undefined; // Truco para "eliminar esa propiedad del objeto antes de devolverlo"
+                            return res.status(200).send({usuario:usuario});
+                        }
+
+                    // si la clave enviada no se corresponde con la clave del usuario registrado
+                    } else {
+                        return res.status(404).send({mensaje:'La clave no es correcta..'});
+                    }
+                }
+             // Si no existe un usuario con el email recibido, devolvemos 404 con mensaje.
+            } else {
+                return res.status(404).send({mensaje:'No existe ningún usuario registrado con ese email.'});
+            }
+    });
+
+}
+
 
 module.exports = {
     inicio,
     prueba,
-    guardarUsuario
+    registrarUsuario,
+    autenticarUsuario
 }
--- api/rutas/usuario.js
+++ api/rutas/usuario.js
@@ -38,10 +38,10 @@ api.get('/prueba',controladorUsuario.prueba);
 
 
 /**
- * Guarda un usuario nuevo.
+ * Registra un usuario nuevo.
  *
- * @name nuevoUsuario
- * @path {POST} /usuario
+ * @name registroNuevoUsuario
+ * @path {POST} /registro
  * @auth No requiere autenticación
  * @body {string} nombre El nombre del nuevo usuario.
  * @body {string} apellidos Los apellidos del nuevo usuario.
@@ -50,7 +50,10 @@ api.get('/prueba',controladorUsuario.prueba);
  * @body {string} clave La clave del nuevo usuario.
  * @body {string} [rol=ROL_USUARIO] El rol del nuevo usuario. Por defecto ROL_USUARIO.
  * @body {string} [image=''] EL archivo de imagen del usuario. Por defecto ''.
- * @code {200} Si el usuario se crea correctamente o si se ha recibido la petición pero faltan datos.
+ * @code {200} Si el usuario se crea correctamente.
+ * @code {200} Si se ha recibido la petición pero faltan datos. o el usuario está duplicado.
+ * @code {200} Si se ha recibido la petición pero el usuario está duplicado.
+ * @code {500} Si hay un error al comprobar duplicados.
  * @code {500} Si hay un error al intentar guardar el nuevo usuario en la bd y no se guarda.
  * @code {404} Si hay un error tras guardar el nuevo usuario en la bd y no se puede encontrar.
  * @response {object} response {}
@@ -64,6 +67,40 @@ api.get('/prueba',controladorUsuario.prueba);
  * @response {string} response.usuario.rol El nombre del usuario creado.
  * @response {string} response.usuario.image El archivo de imagen del usuario creado.
  */
-api.post('/usuario',controladorUsuario.guardarUsuario);
+api.post('/registro',controladorUsuario.registrarUsuario);
+
+/**
+ * Autenticación de un usuario. Si en el body se envía solo email y clave se devuelven los datos del usuario,
+ * si se añade la propiedad enviarToken, se recibe el token JWT de ese usuario, necesario para autenticarse y acceder.
+ *
+ * @name autenticaciónUsuario
+ * @path {POST} /acceso
+ * @auth Requiere email y clave
+ * @body {string} email Email del usuario registrado.
+ * @body {string} clave Clave del usuario registrado.
+ * @body {string} [enviarToken=''] Si se incluye se recibe un token JWT para autenticación, no el usuario.
+ * @code {200} Si el usuario se crea correctamente.
+ * @code {200} Si se ha recibido la petición pero faltan datos. o el usuario está duplicado.
+ * @code {200} Si se ha recibido la petición pero el usuario está duplicado.
+ * @code {500} Si hay un error al comprobar duplicados.
+ * @code {500} Si hay un error al intentar guardar el nuevo usuario en la bd y no se guarda.
+ * @code {404} Si no existe un usuario con el email especificado.
+ * @response {object} response {}
+ * @response {string} response.token El token de autenticación.
+ * @response {object} response.usuario El usuario registrado con ese email y clave. En formato JSON.
+ * @response {string} response.usuario._id El ObjectId del documento en Mongo
+ * @response {string} response.usuario.nombre El nombre del usuario creado.
+ * @response {string} response.usuario.apellidos Los apellidos del usuario creado.
+ * @response {string} response.usuario.apodo El usuario, apodo o nickname del usuario creado.
+ * @response {string} response.usuario.email El nombre del usuario creado.
+ * @response {string} response.usuario.clave El nombre del usuario creado.
+ * @response {string} response.usuario.rol El nombre del usuario creado.
+ * @response {string} response.usuario.image El archivo de imagen del usuario creado.
+ */
+api.post('/acceso',controladorUsuario.autenticarUsuario);
+
+
+
+
 
 module.exports = api;
