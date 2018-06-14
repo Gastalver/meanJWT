38d5b52c5f8342bb82813ca87cb535f58abd97ee
Gastalver <mgastalver@gmail.com>
2018-06-14 02:24:30 +0200

    Creado singleton del servicio usuario en modulo Core.

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppRoutingModule } from './app-routing.module';
+/* App root */
 import { AppComponent } from './app.component';
 
+/* Feature modules */
+import { CoreModule } from './core/core.module';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
+/* Routing module */
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -12,8 +20,9 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
   ],
   imports: [
     BrowserModule,
+    CoreModule.forRoot({apiUrl: '/api'}),
+    NgbModule.forRoot(),
     AppRoutingModule,
-    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
